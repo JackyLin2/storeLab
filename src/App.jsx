40c5a3926233b7d4f96f16b629ef2b4f0c44ce26
JackyLin2 <jacky.jia.lin@gmail.com>
2022-04-18 16:11:32 -0400

    login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Home from "./Pages/Home";
+import ProductList from "./Pages/ProductList";
+import Product from "./Pages/Product";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 
 const App = () => {
     return <div>
-            <Home/>
+            <Login/>
          </div>;
   };
   
